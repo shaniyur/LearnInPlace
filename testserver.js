6d24291eed15359cb3db3e355571e46c861f1b60
miniminivan <54867773+miniminivan@users.noreply.github.com>
2020-07-22 21:50:06 -0500

    created a local host server with node js to stitch the tutor page and home page together

--- testserver.js
+++ testserver.js
@@ -1,12 +1,18 @@
-const http = require("http");
-const { Socket } = require("dgram");
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.write('Hello World');
-        res.end();
-    }
-});
+var express = require('express');
+var path = require('path');
+var app = express();
+// on server this will be http://localhost:post/cssFiles
+app.use('/cssFiles',express.static(__dirname + '/testhomepg/testhomepg/'));
 
-server.listen(3000);
-console.log('Listening on port 3000....');
+app.get('/', function(req, resp) {
+    resp.sendFile('home.html', {root:path.join(__dirname, '../learningalliance/testhomepg/testhomepg')});
+})
 
+app.get(/^(.+)$/, function(req,resp) {
+    console.log(req.params)
+    resp.sendFile(req.params[0], {root:path.join(__dirname)});
+})
+
+app.listen(3000, function() {
+    console.log('listening at port 3000');
+})
