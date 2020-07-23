ee5b710817209e51c4cf3b8a175c620c7eb31ec2
miniminivan <54867773+miniminivan@users.noreply.github.com>
2020-07-23 15:57:39 -0500

    found code to render form to json, submit button no longer works

--- testserver.js
+++ testserver.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser());
 // on server this will be http://localhost:post/cssFiles
 app.use('/cssFiles',express.static(__dirname + '/testhomepg/testhomepg/'));
 
@@ -9,9 +13,21 @@ app.get('/', function(req, resp) {
 })
 
 app.get(/^(.+)$/, function(req,resp) {
-    console.log(req.params)
+    console.log(req.params[0] + ' GET')
     resp.sendFile(req.params[0], {root:path.join(__dirname)});
 })
+
+app.post('/tutorsignups.html', function(req, resp) {
+    console.log(req.params[0] + 'POST');
+    resp.end(JSON.stringify(req.body));
+    console.log(JSON.stringify(req.body));
+    return resp.redirect('/tutorthankyou.html');
+})
+
+// app.post(/^(.+)$/, function(req, resp) {
+//     console.log(req.params[0] + 'POST')
+//     resp.end(JSON.stringify(req.body));
+// })
 
 app.listen(3000, function() {
     console.log('listening at port 3000');
