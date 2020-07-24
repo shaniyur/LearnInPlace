0cee4cdaf0d43775c54ddd92257c00d230bee348
miniminivan <54867773+miniminivan@users.noreply.github.com>
2020-07-24 14:47:42 -0500

    added an extra field person for backend ident

--- renderform.js
+++ renderform.js
@@ -19,7 +19,20 @@ $(function() {
     $('form').submit(function() {
         //const data = $('#result').text(JSON.stringify($('form').serializeObject()));
         // call to back end
-        const data = JSON.stringify($('form').serializeObject());
+        // var newForm = $.extend({}, $('form'), {type:'tutor'})
+        // var data = JSON.stringify(newForm.serializeObject());
+        //$('form').append('<input type="hidden" name="type" value="tutor">');
+        //$( '#tutorsignups' ).append( JSON.stringify( {type: "tutor"} ) ); 
+        //$.extend(this,{type:data});
+        //original one is below
+        //const data = JSON.stringify($('form').serializeObject());
+        //$.extend(data, JSON.stringify({type: 'tutor'}));
+
+        var obj1 = $('form').serializeObject();
+        var obj2 = {person: "tutor"};
+        var data = $.extend({}, obj1, obj2);
+
+        
         $.post('/tutorsubmit', data, function() {
             console.log('Server got tutor data');
         });
