89e39bb107012789653cad01839c89259f992167
PriyankaPai-oss <66020189+PriyankaPai-oss@users.noreply.github.com>
2020-07-28 19:04:43 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
 
-//const try1=document.querySelector("try-this");
+var express = require('express');
+var app = express();
+const bdp=require('body-parser');
 
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
+
+
+app.use(bdp.urlencoded({ extended: true }))
+
+app.get('/',(req,res)=>{
+
+    res.sendFile(__dirname + '/StudentSign.html');
+    
+
 });
 
-sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
-});
 
-// document.getElementById("try-this").addEventListener("click", function(){
-//     alert("Hello World");
-//   });
+    app.post('/tutorthankyou', (req, res) => {
+        console.log(req.body)
+      });
 
-// function toggleForm(){
-//     document.body.classList.toggle('activeForm');
-//   }
+    //   app.post('/StudentAlert', (req, res) => {
+    //     console.log(req.body)
+    //   });
+
+    // app.post('/Send', function(req, res) {
+    //     console.log("BODY"+req.body); //Output=> like { searchid: 'Array of checked checkbox' }
+    //     console.log("SEARCH"+req.body.searchid); // to get array of checked checkbox
+    
+    //    res.sendFile(__dirname + '/test.html')
+    //     //res.send(JSON.stringify(req.body))
+    //     console.log(req.body)
+    //   });
+    app.post('/Send', function (req, res) {
+      console.log(req.body); //Output=> like { searchid: 'Array of checked checkbox' }
+      console.log(req.body.searchid); // to get array of checked checkbox
+  });
+
+    
+
+app.listen(3001, function() {
+    console.log('listening on 3000')
+  });
