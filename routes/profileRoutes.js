d58d8359437797b4e017f28813d064374e860d41
miniminivan <54867773+miniminivan@users.noreply.github.com>
2020-08-11 13:45:59 -0500

    started working on tutor profiles

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,15 +1,16 @@
 const router = require('express').Router();
 
 const authCheck = (req, res, next) => {
-    if(!req.user){
+    if (!req.user) {
         res.redirect('/');
-    }
-    else{
+    } else {
         next();
     }
 };
 
 router.get('/', authCheck, (req, res) => {
+    //res.render('', { user: req.user });
+    console.log(('This is your ' + req.user.userType + ' profile, ' + req.user.firstName))
     res.send('This is your ' + req.user.userType + ' profile, ' + req.user.firstName)
 });
 
