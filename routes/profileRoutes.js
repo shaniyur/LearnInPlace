04bacec3241c5768ac443a1a5a3122c4d2f603a7
miniminivan <54867773+miniminivan@users.noreply.github.com>
2020-08-12 14:07:46 -0500

    fixed profile routes

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -10,14 +10,12 @@ const authCheck = (req, res, next) => {
 };
 
 router.get('/', authCheck, (req, res) => {
-    res.render('../views/layouts/index.handlebars', { first: req.user.firstName });
-    // console.log(('This is your ' + req.user.userType + ' profile, ' + req.user.firstName))
-    //res.send('This is your ' + req.user.userType + ' profile, ' + req.user.firstName)
-    // if (!req.user) {
-    //     res.send("you're not registered");
-    // } else {
-    //     res.render('../views/layouts/index.handlebars', { first: req.user.firstName });
-    // }
+    console.log(('This is your ' + req.user.userType + ' profile, ' + req.user.firstName))
+    if (req.user.userType === "Tutor") {
+        res.render('../views/layouts/index.handlebars', { first: req.user.firstName });
+    } else {
+        res.send('This is your ' + req.user.userType + ' profile, ' + req.user.firstName)
+    }
 });
 
 module.exports = router
