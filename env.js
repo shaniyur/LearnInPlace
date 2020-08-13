9212e33f054a70b59615f725df24bab177a8e086
miniminivan <54867773+miniminivan@users.noreply.github.com>
2020-08-13 18:11:49 -0500

    update

---
+++ env.js
@@ -0,0 +1,7 @@
+const DATABASE_URI = 'mongodb+srv://dbUser:summer2020@cluster0.hropv.mongodb.net/test?retryWrites=true&w=majority'; 
+const EMAIL_PASSWORD = 'Team2020';
+const EMAIL_ADDRESS = 'learninplaceteam@gmail.com';
+const CLIENT_ID = '418625314255-hdade0ln2b0pbeal8bs7olfaj3v4e4hc.apps.googleusercontent.com';
+const CLIENT_SECRET = 'VF7Tn91paKeT_tNQl9MpF81Z';
+
+module.exports = {DATABASE_URI, EMAIL_PASSWORD, EMAIL_ADDRESS, CLIENT_ID, CLIENT_SECRET}
